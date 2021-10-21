6428b0fc318ad8ecf75e054ba97bd4b0d08f1caa
andrii2706 <83228054+andrii2706@users.noreply.github.com>
2021-10-21 16:31:37 +0300

    defolt

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex justify-center item-center bg-green-200">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
